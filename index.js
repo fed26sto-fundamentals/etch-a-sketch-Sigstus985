f8364f74a41816faaaed615e658d7a5ac2d2fa8c
Sigstus985 <sixten.turden@hyperisland.se>
2024-09-16 13:41:13 +0200

    fixed up the HTML, added CSS and wrote the JS

--- index.js
+++ index.js
@@ -1,6 +1,49 @@
-function spawnGrid () {
-    for (let i = 0; i < 16; i++){
-    const divs = document.createElement("div");
-    document.getElementById("container").appendChild(divs);
+let color = "black"
+
+//Spawning in the grid
+function populateBoard(size) {
+    let board = document.querySelector(".board");
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => div.remove());
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let amount = size * size;
+    for (let i = 0; i < amount; i++) {
+        let square = document.createElement("div");
+        square.addEventListener("mouseover", colorSquare);
+        square.style.backgroundColor = "gray";
+        board.insertAdjacentElement("beforeend", square);
     }
+}
+
+populateBoard(16);
+
+//Change the amount of divs in the grid
+function changeSize(input) {
+    if(input <= 1 || input >= 101 ){
+        console.log("Invalid input");
+    } else {
+        populateBoard(input);
+    }
+} 
+
+//changing color
+function changeColor(choice) {
+    color = choice;
+}
+
+//Colouring in the divs
+function colorSquare() {
+    if ((color == "Random")) {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else{
+        this.style.backgroundColor = color;
+    }
+}
+
+function resetBoard() {
+    let board = document.querySelector(".board");
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => div.style.backgroundColor = "gray");
 }
